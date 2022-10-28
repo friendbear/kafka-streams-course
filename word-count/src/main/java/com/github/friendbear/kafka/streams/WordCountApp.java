981b3bba251762875b87d66e3594e50ff6475697
T Kumagai <4956799+friendbear@users.noreply.github.com>
2022-10-28 21:35:11 +0900

    My first kafka stream

---
+++ word-count/src/main/java/com/github/friendbear/kafka/streams/WordCountApp.java
@@ -0,0 +1,60 @@
+package com.github.friendbear.kafka.streams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCountApp {
+
+    public static Topology createTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+
+        var wordCounts = wordCountInput.mapValues(value -> value.toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((ignoredKey, word) -> word)
+                .groupByKey()
+                .count(Materialized.as("Counts"));
+
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        return builder.build();
+
+    }
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KafkaStreams streams = new KafkaStreams(WordCountApp.createTopology(), properties);
+        streams.start();
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        while(true) {
+            streams.metadataForLocalThreads().forEach(data -> {
+                System.out.println(data);
+            });
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
